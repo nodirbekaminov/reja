303db8c4e7528886c005efba88dfe2c7cd7646a6
nodirbekaminov <nodiraminov0101@gmail.com>
2026-09-06 01:10:13 +0500

    feat: Completed  FrontEnd JS

--- app.js
+++ app.js
@@ -1,63 +1,59 @@
-console.log('Web serverni boshlash!');
-const express = require('express');
+console.log("Web serverni boshlash!");
+const express = require("express");
 const app = express();
 
 // MongoDB chaqirib olish
-const db = require('./server').db();
+const db = require("./server").db();
 
-const fs = require('fs');
+const fs = require("fs");
 
 let user;
-fs.readFile('database/user.json', 'utf8', (err, data) => {
+fs.readFile("database/user.json", "utf8", (err, data) => {
   if (err) {
-    console.log('Error:', err);
+    console.log("Error:", err);
   } else {
     user = JSON.parse(data);
   }
 });
 // 1: Kirish Code
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session Code
 
 // 3: Views Code
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 // 4: Routing Code
-app.post('/create-item', (req, res) => {
-  console.log('user entered /create-item');
+app.post("/create-item", (req, res) => {
+  console.log("user entered /create-item");
   console.log(req.body);
   const new_reja = req.body.reja;
-  db.collection('plans').insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end('Something went wrong!');
-    } else {
-      res.end('Successfully added!');
-    }
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
-app.get('/', function (req, res) {
-  console.log('user entered /');
-  db.collection('plans')
+app.get("/", function (req, res) {
+  console.log("user entered /");
+  db.collection("plans")
     .find()
     .toArray((err, data) => {
       if (err) {
         console.log(err);
-        res.end('Something went wrong!');
+        res.end("Something went wrong!");
       } else {
         console.log(data);
-        res.render('reja', { items: data });
+        res.render("reja", { items: data });
       }
     });
 });
 
-app.get('/author', (req, res) => {
-  res.render('author', { user: user });
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 module.exports = app;
--- train.js
+++ train.js
@@ -1,3 +1,37 @@
+// TASK B
+
+// const count_numbers = (str) => {
+//   let count = 0;
+//   let i = 0;
+
+//   while (i < str.length) {
+//     let char = str[i];
+
+//     if (char >= "0" && char <= "9") {
+//       count++;
+//     }
+
+//     i++;
+//   }
+
+//   return count;
+// };
+
+// const test_strings = [
+//   "ad2a54y79wet0sfgb9",
+//   "test123code456",
+//   "hello99world00",
+//   "hello1MITA7andMIT8",
+// ];
+
+// test_strings.forEach((str) => {
+//   console.log(`"${str}" ichidagi raqamlar soni:`, count_numbers(str));
+// });
+
+// *************************************************************************************************
+
+// TASK A
+
 // const list = [
 //   "Bu so'zda 2 marta va undan ko'p ishlatilgan!",
 //   "Bu so'zda 1 marta ishlatilgan!",
@@ -34,35 +68,3 @@
 // char_count('mississippi');
 // console.log('***********************************************');
 // char_count('infraztuzilma');
-
-// *************************************************************************************************
-
-// TASK B
-
-const count_numbers = (str) => {
-  let count = 0;
-  let i = 0;
-
-  while (i < str.length) {
-    let char = str[i];
-
-    if (char >= '0' && char <= '9') {
-      count++;
-    }
-
-    i++;
-  }
-
-  return count;
-};
-
-const test_strings = [
-  'ad2a54y79wet0sfgb9',
-  'test123code456',
-  'hello99world00',
-  'hello1MITA7andMIT8',
-];
-
-test_strings.forEach((str) => {
-  console.log(`"${str}" ichidagi raqamlar soni:`, count_numbers(str));
-});
